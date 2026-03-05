648b858721feadd7cbd85354a3c56329e3945358
tlagu004.fiu <tlagu004.fiu@gmail.com>
2026-03-05 15:48:12 +0000

    Connect to MongoDB & Create Collections & Insert First User

--- mongodb.py
+++ mongodb.py
@@ -3,10 +3,18 @@ from pymongo import MongoClient
 import os
 
 load_dotenv()
+MONGO_URL = os.environ.get("MONGO_URL")
 MONGO_USER = os.environ.get("MONGO_USER")
 MONGO_PASSWORD = os.environ.get("MONGO_PASS")
 MONGO_CLUSTER_URL = os.environ.get("MONGO_CLUSTER_URL")
-
-print(MONGO_USER)
-print(MONGO_PASSWORD)
-print(MONGO_CLUSTER_URL)
+client=MongoClient(MONGO_URL)
+db = client["netlify"]
+collection = db["users"]
+user1 = {
+    "firstName":"john",
+    "lastName":"doe",
+    "email":"jdoe305@gmail.com"
+}
+result1=collection.insert_one(user1)
+print(client)
+print(collection)
