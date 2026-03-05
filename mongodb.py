8c32d57504f895ba5bf5cbbdb44f718cb168ce9e
tlagu004.fiu <tlagu004.fiu@gmail.com>
2026-03-05 16:06:28 +0000

    Insert Multiple Users

--- mongodb.py
+++ mongodb.py
@@ -10,11 +10,40 @@ MONGO_CLUSTER_URL = os.environ.get("MONGO_CLUSTER_URL")
 client=MongoClient(MONGO_URL)
 db = client["netlify"]
 collection = db["users"]
-user1 = {
-    "firstName":"john",
-    "lastName":"doe",
-    "email":"jdoe305@gmail.com"
-}
-result1=collection.insert_one(user1)
-print(client)
-print(collection)
+users = [
+    {
+        "firstName": "Jane",
+        "lastName": "Smith",
+        "email": "jsmith_dev@outlook.com",
+        # "username": "janey_codes"
+    },
+    {
+        "firstName": "Carlos",
+        "lastName": "Rodriguez",
+        "email": "c.rodriguez@miami.edu",
+        # "username": "crod_305"
+    },
+    {
+        "firstName": "Aisha",
+        "lastName": "Khan",
+        "email": "aisha.k@techcorp.io",
+        # "username": "ak_cloud"
+    },
+    {
+        "firstName": "Liam",
+        "lastName": "O'Connor",
+        "email": "liam.oconnor@protonmail.com",
+        # "username": "luckyliam"
+    },
+    {
+        "firstName": "Yuki",
+        "lastName": "Tanaka",
+        "email": "y.tanaka@global.jp",
+        # "username": "yuki_t"
+    }
+]
+result1=collection.insert_many(users)
+findJane = collection.find({"firstName":"john"})
+print(findJane)
+# print(client)
+# print(collection)
